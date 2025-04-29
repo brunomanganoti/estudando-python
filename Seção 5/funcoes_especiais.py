3c1b8cd6ebbfb5585f60252dd83a105e886eace7
Bruno Manganoti <brunomanganoti@gmail.com>
2025-04-29 20:51:15 -0300

    funções especiais e compreensões de lista

--- Seção 5/funcoes_especiais.py
+++ Seção 5/funcoes_especiais.py
@@ -21,6 +21,45 @@ print(f_c(32)) """
 dobro = list(map(lambda x: x*2, num))
 print(dobro) """
 
-palavras = ['Python', 'Linguagem', 'Programação']
+""" palavras = ['Python', 'Linguagem', 'Programação']
 up = " ".join(list(map(str.upper, palavras)))
-print(up)
+print(up) """
+
+# Função filter
+# Sintaxe:
+# filter(função, sequência)
+
+def ehPar(num):
+    return num % 2 == 0
+
+nums = [1,2,3,4,5,6,7,8,9,10,24]
+
+""" nums_par = list(filter(ehPar, nums))
+print(nums_par) """
+
+""" nums_impar = list(filter(lambda n: n % 2 != 0, nums))
+print(nums_impar) """
+
+# Função reduce()
+# Sintaxe:
+# reduce(função, sequência, valor_inicial)
+
+from functools import reduce
+
+""" def mult(x, y):
+    return x * y
+
+numeros = [1,2,2,3,4,6]
+
+nums_mult = reduce(mult, numeros)
+print(nums_mult) """
+
+# Soma cumulativa dos quadrados de valores usando expressão lambda
+
+numeros = [1,2,3,4]
+
+def soma(x, y):
+    return x + y
+
+soma_quadrados = reduce(lambda x, y: x**2 + y**2, numeros)
+print(soma_quadrados)
