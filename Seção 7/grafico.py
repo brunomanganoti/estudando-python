24077ea575c3630ac10aab149a5c023653c7173f
Bruno Manganoti <brunomanganoti@gmail.com>
2025-05-17 10:39:02 -0300

    matplotlib pt. 2

--- Seção 7/grafico.py
+++ Seção 7/grafico.py
@@ -10,12 +10,23 @@ plt.title('Temperaturas máximas e mínimas')
 plt.xlabel('Dias')
 plt.ylabel('Temperaturas')
 
-plt.plot(eixo_x_dias, eixo_y_tempmax, linestyle='--', marker='o')
-plt.plot(eixo_x_dias, eixo_y_tempmin, color='#1ddfa5', linewidth=1)
+plt.bar(eixo_x_dias, eixo_y_tempmax, label='Temp Máx')
+plt.plot(eixo_x_dias, eixo_y_tempmin,color='#550099',label='Temp Mín')
 
 # primeiro plot = primeira legenda
-plt.legend(['Temp Máx.', 'Temp Mín.'])
+plt.legend(loc='best')
 
 # plt.grid(True)
 # print(plt.style.available)
+
+""" plt.axis([1,31,5,45])
+print(plt.axis()) """
+""" plt.ylim([-5,50])
+plt.xlim([1,31]) """
+# plt.savefig('grafico.png')
+
+# Plotar gráfico de uma função como f(x) = x^3 + 1
+""" x = range(1,11,1)
+plt.plot(x, [(num**3 + 1) for num in x]) """
+
 plt.show()
