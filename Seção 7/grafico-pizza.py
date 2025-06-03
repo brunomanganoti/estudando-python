6751e31212c7c2a5c144fc960596b80dc2fb45e2
Bruno Manganoti <brunomanganoti@gmail.com>
2025-06-03 17:46:12 -0300

    matplotlib pt. 4.1

---
+++ Seção 7/grafico-pizza.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+linguagens = ['Python', 'JavaScript', 'C#', 'Ruby']
+quantidades = [300,270,150,40]
+
+plt.pie(quantidades, labels=linguagens, autopct='%1.1f%%', startangle=90)
+
+plt.title('Preferências por linguagens de programação')
+plt.legend(title='Linguagens', loc='best', bbox_to_anchor=(1, .8))
+
+plt.show()
