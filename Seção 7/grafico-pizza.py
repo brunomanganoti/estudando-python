ed6125aaf7ab22addab8ce7da0db29b4da0effd9
Bruno Manganoti <brunomanganoti@gmail.com>
2025-06-04 23:24:04 -0300

    matplotlib pt. 4.2

--- Seção 7/grafico-pizza.py
+++ Seção 7/grafico-pizza.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 
 linguagens = ['Python', 'JavaScript', 'C#', 'Ruby']
 quantidades = [300,270,150,40]
+cores = ('#660033', 'blue', 'yellow', 'g')
+distancia = (0, 0, 0.1, 0.1)
 
-plt.pie(quantidades, labels=linguagens, autopct='%1.1f%%', startangle=90)
+plt.pie(quantidades, labels=linguagens, autopct='%1.1f%%', startangle=90, shadow=True, colors=cores, explode=distancia)
 
 plt.title('Preferências por linguagens de programação')
 plt.legend(title='Linguagens', loc='best', bbox_to_anchor=(1, .8))
